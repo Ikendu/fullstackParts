5c2da4ebde79c4ee85665ca78950f4875d2c431f
Ikendu <ndubest56@gmail.com>
2026-06-29 20:08:44 +0100

    project 1.7 done

--- part1project/src/App.jsx
+++ part1project/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positivePercentage = (good / total) * 100 || 0;
+
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average: {average.toFixed(2)}</p>
+      <p>Positive: {positivePercentage.toFixed(2)}%</p>
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  let checker = good || neutral || bad;
 
   return (
     <div>
@@ -12,14 +31,16 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-
       <div>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
+        <h2>Statistics</h2>
+        {checker > 0 ? (
+          <Statistics good={good} neutral={neutral} bad={bad} />
+        ) : (
+          <h4>No feedback given</h4>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
