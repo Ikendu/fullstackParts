43d9ab32ec19e51f1cf7791b351050a1e9476f3c
Ikendu <ndubest56@gmail.com>
2026-07-04 16:35:59 +0100

    static page use express

--- part2/phonebook/phonebook.js
+++ part2/phonebook/phonebook.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000/api/phonebook";
+// const baseUrl = "http://localhost:3000/api/phonebook";
+const baseUrl = "/api/phonebook";
 
 export const getAll = async () => {
   const response = await axios.get(baseUrl);
