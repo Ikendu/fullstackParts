9419a3bd2090518992a373cc20950a48101906f8
Ikendu <ndubest56@gmail.com>
2026-07-13 06:41:07 +0100

    list testing

--- Algorithm/reorderLinkedList.js
+++ Algorithm/reorderLinkedList.js
@@ -1,11 +1,42 @@
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
-function reorderList(head) {
+function createLinkedList(arr) {
+  if (arr.length == 0) return;
+
+  let head = new Node(arr[0]);
+  let current = head;
+
+  for (let i = 1; i < arr.length; i++) {
+    current.next = new Node(arr[i]);
+    current = current.next;
+  }
+  return head;
+}
+
+function printList(head) {
+  if (!head) return [];
+
+  let arryalist = [];
+
+  let current = head;
+
+  while (current) {
+    arryalist.push(current.value);
+    current = current.next;
+  }
+  return arryalist;
+}
+
+console.log(createLinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(reorderList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+function reorderList(arr) {
+  let head = createLinkedList(arr);
   if (!head.next || !head.next.next) return;
 
   let fast = head;
@@ -19,28 +50,30 @@ function reorderList(head) {
   let second = slow.next;
   slow.next = null;
 
-  let prev = null
+  let prev = null;
 
-  while(second.next){
-    let next = second.next
-    second.next = prev
-    
-    prev = second
-    second = next
+  while (second.next) {
+    let next = second.next;
+    second.next = prev;
+
+    prev = second;
+    second = next;
   }
-  second = prev
+  second = prev;
 
-  let first = head
+  let first = head;
 
-  while(second){
-    let temp1 = first.next
-    let temp2 = second.next
+  while (second) {
+    let temp1 = first.next;
+    let temp2 = second.next;
 
-    first.next = second
-    second.next = temp1
+    first.next = second;
+    second.next = temp1;
 
-    first = temp1
-    second = temp2
+    first = temp1;
+    second = temp2;
   }
 
+  return printList(head);
+  //   return head;
 }
