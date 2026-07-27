3d4921e3b8600659f866396cca16359dfeb148b5
Ikendu <ndubest56@gmail.com>
2026-07-27 07:51:37 +0100

    linked list creation

--- Algorithm/linkedLists/listStructure.js
+++ Algorithm/linkedLists/listStructure.js
@@ -26,4 +26,31 @@ function printList(head) {
 let list = createList([3, 4, 6, 8, 10, 5]);
 printList(list);
 
+function insertFirst(head, value) {
+  if (!head) head = new ListNode(value);
+  else {
+    let node = new ListNode(value);
+    node.next = head;
+    head = node;
+  }
+  return head;
+}
 
+function deleteFirst(head) {
+  if (!head) return null;
+  return head.next;
+}
+
+function insertEnd(head, value) {
+  let node = new ListNode(value);
+  if (!head) {
+    return node;
+  }
+  let current = head;
+
+  while (current.next) {
+    current = current.next;
+    current.next = node;
+  }
+  return head;
+}
